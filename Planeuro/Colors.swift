db56be7d777f69b3917ccea06f5f061794b45c2a
elinnlle <matveenkoelvira307@gmail.com>
2025-01-18 19:12:31 +0300

    Fonts and colors

---
+++ Planeuro/Colors.swift
@@ -0,0 +1,23 @@
+//
+//  Colors.swift
+//  Planeuro
+//
+//  Created by Эльвира Матвеенко on 16.01.2025.
+//
+
+import UIKit
+
+extension UIColor {
+    static let white = UIColor.white
+    static let color100 = UIColor(red: 238/255, green: 235/255, blue: 247/255, alpha: 1) // #EEEBF7
+    static let color200 = UIColor(red: 226/255, green: 222/255, blue: 246/255, alpha: 1) // #E2DEF6
+    static let color300 = UIColor(red: 202/255, green: 192/255, blue: 235/255, alpha: 1) // #CAC0EB
+    static let color400 = UIColor(red: 175/255, green: 159/255, blue: 224/255, alpha: 1) // #AF9FE0
+    static let color500 = UIColor(red: 149/255, green: 123/255, blue: 212/255, alpha: 1) // #957BD4
+    static let color600 = UIColor(red: 125/255, green: 87/255, blue: 194/255, alpha: 1) // #7D57C2
+    static let color700 = UIColor(red: 107/255, green: 68/255, blue: 172/255, alpha: 1) // #6B44AC
+    static let color800 = UIColor(red: 91/255, green: 60/255, blue: 140/255, alpha: 1) // #5B3C8C
+    static let color900 = UIColor(red: 71/255, green: 47/255, blue: 110/255, alpha: 1) // #472F6E
+    static let color950 = UIColor(red: 47/255, green: 25/255, blue: 79/255, alpha: 1) // #2F194F
+    static let black = UIColor.black
+}
